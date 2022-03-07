50d5d15f2ff3016c7db481299c4ddadfe1227666
savent <savent_gate@outlook.com>
2022-03-07 17:40:56 +0800

    first commit

---
+++ inc/main.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.hpp
+ * @author savent (savent_gate@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * Copyright 2022 savent_gate
+ * 
+ */
+ #pragma once
+
+ #include <cstdint>
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @author savent (savent_gate@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * Copyright 2022 savent_gate
+ * 
+ */
+ #include "main.hpp"
+
+ int main() {
+     return 0;
+ }
